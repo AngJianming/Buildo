1912bc00640ce137987958b6cd09546056b42d73
AngJianming <angjianming2005@hotmail.com>
2025-01-21 09:16:10 +0800

    update

--- src/app.ts
+++
@@ -1,7 +0,0 @@
-import { eachArray } from './utils';  // Adjust the import path according to your directory structure
-
-const arr = [1, 2, 3];
-eachArray(arr, (item, index) => {
-    console.log(index, item);
-    return item;
-});
